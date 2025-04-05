21c2ec859c8875126455af39e72295085bf9e0de
Shane <6071159+smashedr@users.noreply.github.com>
2025-04-05 12:09:42 -0700

    Python

--- tst.py
+++ tst.py
@@ -4,4 +4,6 @@ TIMEZONE_CHOICES = zip(
     sorted(zoneinfo.available_timezones()), sorted(zoneinfo.available_timezones()), strict=False
 )
 
-print(TIMEZONE_CHOICES)
+print(zoneinfo.available_timezones())
+print('-'*40)
+print(dict(TIMEZONE_CHOICES))
