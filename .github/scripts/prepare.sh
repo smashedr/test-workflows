39ab2291ae20686caee2f847763721916faeaf80
Shane <6071159+smashedr@users.noreply.github.com>
2025-04-30 19:45:52 -0700

    Push

--- .github/scripts/prepare.sh
+++ .github/scripts/prepare.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-VERSION="11.11.1"
+HLJS_VERSION="11.11.1"
 
 which git || (echo "Missing: git" && exit 1)
 which npm || (echo "Missing: npm" && exit 1)
@@ -11,14 +11,19 @@ which node || (echo "Missing: node" && exit 1)
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "SCRIPT_DIR: ${SCRIPT_DIR}"
-SOURCE_DIR="${SCRIPT_DIR}/../highlight"
+
+echo "Processing highlight.js: ${HLJS_VERSION}"
+
+SOURCE_DIR="${SCRIPT_DIR}/../highlightjs"
 echo "SOURCE_DIR: ${SOURCE_DIR}"
 TARGET_DIR="${SCRIPT_DIR}/../../app/src/main/assets/preview/dist"
 echo "TARGET_DIR: ${TARGET_DIR}"
 
-git clone https://github.com/highlightjs/highlight.js.git "${SOURCE_DIR}"
-cd "${SOURCE_DIR}"
-git checkout "${VERSION}"
+if [ ! -d "${SOURCE_DIR}" ];then
+  git clone https://github.com/highlightjs/highlight.js.git "${SOURCE_DIR}"
+  cd "${SOURCE_DIR}"
+  git checkout "${HLJS_VERSION}"
+fi
 
 npm install
 node tools/build.js :common
@@ -28,4 +33,6 @@ cp build/highlight.min.js "${TARGET_DIR}"
 cp src/styles/stackoverflow-dark.css "${TARGET_DIR}"
 cp src/styles/stackoverflow-light.css "${TARGET_DIR}"
 
-echo "Finished."
+echo "Finished: highlight.js"
+
+echo "Everything is finished."
