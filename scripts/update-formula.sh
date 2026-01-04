2cfdbc8d157100e15b80adb6e2d8eb260d81676b
Shane <6071159+smashedr@users.noreply.github.com>
2026-01-03 21:37:57 -0800

    test 3 - 1

--- scripts/update-formula.sh
+++ scripts/update-formula.sh
@@ -5,10 +5,16 @@ set -e
 key="${1}"
 value="${2}"
 
-echo "key: '${key}'"
-echo "value: '${value}'"
+if [ -z "${value}" ];then
+    echo "Skipping ${key} - no value provided."
+fi
+
+echo "::group::Processing: ${key}"
+echo "value: ${value}"
 
 # shellcheck disable=SC2154
 line=$(grep -n -m1 '^[[:space:]]*'"${key} " "${formula}" | cut -f1 -d:)
 echo "line: ${line}"
 sed -i "${line}"'s,.*,'"  url \"${value}\"," "${formula}"
+
+echo "::endgroup::"
